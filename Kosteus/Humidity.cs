28960eb4e6ddddd80230da3943c2e2bcf97494d4
Saamari <sampsa_kautto@hotmail.com>
2016-03-22 12:11:39 +0200

    d

---
+++ Kosteus/Humidity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kosteus
+{
+    class Humidity
+    {
+        private readonly double minhumidity;
+        private readonly double maxhumidity;
+
+        private double min;
+        private double max;
+
+        public double Min
+
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < minhumidity) min = value;
+            }
+        }
+
+            public double Max
+        {
+            get
+            {
+                return max;
+            }
+            set
+            {
+                if (value > maxhumidity) max = value;
+            }
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
